8a3facceff99e2f3ccfbca2a412a6c940a4e814c
Alejandro Marano <alejandromarano08@gmail.com>
2017-02-08 20:13:29 -0300

    8.13Hs
    -Se agrego la fun multiplicar

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int multiplicar(int a, int b);
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d",multiplicar(3,3));
+
     return 0;
+}
+
+int multiplicar(int a, int b)
+{
+    int resultado=0;
+    resultado=a*b;
+    return resultado;
 }
